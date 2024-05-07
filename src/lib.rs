d0570d5bd78e967124a62d9d3282bb250deb9717
alion02 <alojczek@gmail.com>
2024-05-07 22:31:50 +0200

    Rename entry to slot

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,13 @@ use core::{
 
 use key::Key;
 
-union Entry<T, K: Key> {
+union Slot<T, K: Key> {
     val: ManuallyDrop<T>,
     next: K,
     uninit: (),
 }
 
-impl<T, K: Key> Entry<T, K> {
+impl<T, K: Key> Slot<T, K> {
     #[inline]
     fn uninit() -> Self {
         Self { uninit: () }
@@ -27,7 +27,7 @@ impl<T, K: Key> Entry<T, K> {
 }
 
 pub struct Slab<T, K: Key = u32> {
-    entries: Box<[Entry<T, K>]>,
+    slots: Box<[Slot<T, K>]>,
     next: K,
     len: K,
 }
@@ -37,7 +37,7 @@ impl<T, K: Key> Slab<T, K> {
     #[must_use]
     pub fn new() -> Self {
         Self {
-            entries: Box::new([]),
+            slots: Box::new([]),
             next: K::ZERO,
             len: K::ZERO,
         }
@@ -46,38 +46,38 @@ impl<T, K: Key> Slab<T, K> {
     #[inline(never)]
     fn extend(&mut self) {
         const INITIAL_SIZE: usize = 4;
-        let ptr = &mut self.entries as *mut Box<[_]>;
+        let ptr = &mut self.slots as *mut Box<[_]>;
         unsafe {
             let b = ptr::read(ptr);
             let extend_by = if b.len() == 0 { INITIAL_SIZE } else { b.len() };
             let mut vec = b.into_vec();
-            vec.extend(repeat_with(Entry::uninit).take(extend_by));
+            vec.extend(repeat_with(Slot::uninit).take(extend_by));
             ptr::write(ptr, vec.into_boxed_slice());
         }
     }
 
     /// # Safety
     ///
-    /// The number of occupied entries must be lower than the maximum value of `K`. This is
-    /// trivially true if `K` is [`usize`].
+    /// The number of occupied slots must be lower than the maximum value of `K`. This is trivially
+    /// true if `K` is [`usize`].
     #[inline]
     pub unsafe fn insert(&mut self, val: T) -> K {
         let next = self.next;
 
-        if next.as_usize() == self.entries.len() {
+        if next.as_usize() == self.slots.len() {
             self.extend();
         }
 
-        let entry = unsafe { self.entries.get_unchecked_mut(next.as_usize()) };
+        let slot = unsafe { self.slots.get_unchecked_mut(next.as_usize()) };
 
         self.next = if self.next == self.len {
             self.next.inc()
         } else {
-            unsafe { entry.next }
+            unsafe { slot.next }
         };
         self.len = self.len.inc();
 
-        entry.val = ManuallyDrop::new(val);
+        slot.val = ManuallyDrop::new(val);
 
         next
     }
@@ -90,13 +90,13 @@ impl<T, K: Key> Slab<T, K> {
     /// between the insertion and this call.
     #[inline]
     pub unsafe fn remove(&mut self, key: K) -> T {
-        let entry = unsafe { self.entries.get_unchecked_mut(key.as_usize()) };
-        let entry = mem::replace(entry, Entry { next: self.next });
+        let slot = unsafe { self.slots.get_unchecked_mut(key.as_usize()) };
+        let slot = mem::replace(slot, Slot { next: self.next });
 
         self.next = key;
         self.len = self.len.dec();
 
-        ManuallyDrop::into_inner(unsafe { entry.val })
+        ManuallyDrop::into_inner(unsafe { slot.val })
     }
 
     /// # Safety
@@ -105,7 +105,7 @@ impl<T, K: Key> Slab<T, K> {
     /// between the insertion and this call.
     #[inline]
     pub unsafe fn get(&self, key: K) -> &T {
-        unsafe { &self.entries.get_unchecked(key.as_usize()).val }
+        unsafe { &self.slots.get_unchecked(key.as_usize()).val }
     }
 
     /// # Safety
@@ -114,7 +114,7 @@ impl<T, K: Key> Slab<T, K> {
     /// between the insertion and this call.
     #[inline]
     pub unsafe fn get_mut(&mut self, key: K) -> &mut T {
-        unsafe { &mut self.entries.get_unchecked_mut(key.as_usize()).val }
+        unsafe { &mut self.slots.get_unchecked_mut(key.as_usize()).val }
     }
 
     /// Get the number of elements contained within this [`Slab`].
